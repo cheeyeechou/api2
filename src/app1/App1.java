7f984b16139c155c6d4a14349f5cbed62d2a27d5
CarmenLL68 <carmen_68@live.com.my>
2015-06-15 04:25:39 +0800

    new commit

--- src/app1/App1.java
+++ src/app1/App1.java
@@ -1,19 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package app1;
 
-/**
- *
- * @author Carmen
- */
+
 public class App1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = 3, b = 2, c = 1;
+        
+        if (a > c){
+            System.out.println(b);
+        }
     }
 }
